e920bddf9a6dbe04c1d4529dea058cfbc30f7c92
andres26142 <andres26142@gmail.com>
2021-07-26 15:55:55 -0500

    Update UTestSignUpPage.java
    Se elimino el import innecesario en la userinterface UTestSignUpPage.java

--- src/main/java/co/com/choucair/retotecnico/userinterface/UTestSignUpPage.java
+++ src/main/java/co/com/choucair/retotecnico/userinterface/UTestSignUpPage.java
@@ -1,6 +1,6 @@
 package co.com.choucair.retotecnico.userinterface;
 
-import net.serenitybdd.core.pages.PageObject;
+
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
